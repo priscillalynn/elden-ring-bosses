b83ed7eae5771d77ab99149f08a7247d8f17c544
priscillalynn <plynn9036@gmail.com>
2023-09-18 13:03:15 -0400

    wip

--- client/public/scripts/boss.js
+++ client/public/scripts/boss.js
@@ -3,26 +3,17 @@ const renderBoss = async () => {
 
   const response = await fetch("/bosses");
   const data = await response.json();
-
   const bossContent = document.getElementById("boss-content");
-
-  let boss;
-
-  boss = data.find((boss) => boss.id === requestedID);
+  let boss = data.find((boss) => boss.id === requestedID);
 
   if (boss) {
     document.getElementById("image").src = boss.image;
     document.getElementById("name").textContent = boss.name;
-    /*
-    document.getElementById("submittedBy").textContent =
-      "Submitted by: " + gift.submittedBy;
-    document.getElementById("pricePoint").textContent =
-      "Price: " + gift.pricePoint;
-    document.getElementById("audience").textContent =
-      "Great For: " + gift.audience;
-    document.getElementById("description").textContent = gift.description;
-    document.title = `UnEarthed - ${gift.name}`;
-    */
+    document.getElementById("health").textContent = boss.health;
+    document.getElementById("defense").textContent = boss.defense;
+    document.getElementById("type").textContent = boss.type;
+    document.getElementById("lore").textContent = boss.lore;
+    document.title = `Elden Ring Bosses - ${boss.name}`;
   } else {
     const message = document.createElement("h2");
     message.textContent = "No Boss Found 😞";
--- server/data/bosses.js
+++ server/data/bosses.js
@@ -11,13 +11,13 @@ const bosses = [
   },
   {
     id: 2,
-    name: "Margit the Fell Omen",
-    health: "4,174 HP",
-    defense: "103",
+    name: "Godrick the Grafted",
+    health: "6,080 HP",
+    defense: "105",
     type: "optional",
     image:
-      "https://static.wikia.nocookie.net/eldenring/images/d/d0/ER_Margit_Boss.png/revision/latest?cb=20230627194208",
-    lore: "Margit is an Omen who gained notoriety during The Shattering, in which he slaughtered countless champions who harbored ambitions for Lordship, stacking high their corpses during the Second Defense of Leyndell.",
+      "https://assetsio.reedpopcdn.com/elden-ring-godrick.jpg?width=1600&height=900&fit=crop&quality=100&format=png&enable=upscale&auto=webp",
+    lore: "Godrick is a descendant of the Golden Lineage, the bloodline which began with Queen Marika the Eternal and her first consort Godfrey. Only a distant relation of Marika's line, his divine blood was sorely diluted, and he was viewed as the runt of the litter.",
   },
 ];
 
