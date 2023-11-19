61b2cc3df9c6cd3fbc443be0d15ef2f6185aa13d
priscillalynn <plynn9036@gmail.com>
2023-11-19 12:25:21 -0500

    made header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,23 @@
+import "../styles/index.css";
+
 const Header = () => {
+  const headerStyle = {
+    backgroundImage:
+      "url('https://backend.artreview.com/wp-content/uploads/2022/03/heroDn3SrrTeZNNq5x62Rn4bhc.jpg')",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    className: "bg-image",
+  };
+
   return (
-    <div className="bg-black">
-      <h1 className="text-white p-28">Elden Ring Bosses</h1>
+    <div className="text-white p-32" style={headerStyle}>
+      <a href="#" className="text-4xl font-extrabold">
+        Elden Ring Bosses
+      </a>
+      <h6>
+        For when you're tired of dying to them <span>💀</span>
+      </h6>
     </div>
   );
 };
