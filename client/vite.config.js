73ee0560700cc4b7f3b49e4f77be2d8a362f1214
priscillalynn <plynn9036@gmail.com>
2023-11-18 19:44:37 -0500

    add dependencies

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "../server/public",
+    emptyOutDir: true,
+  },
+  server: {
+    proxy: {
+      "/bosses": {
+        target: "http://localhost:3001",
+      },
+    },
+  },
+});
