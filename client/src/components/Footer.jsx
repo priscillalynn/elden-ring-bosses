d85bc861238d8b7c01e4155772d0e2e7a2c48660
priscillalynn <plynn9036@gmail.com>
2023-11-19 12:25:31 -0500

    made footer component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,15 +1,16 @@
 import { Link } from "@nextui-org/react";
-import { Layers3 } from "lucide-react";
 
 const Footer = () => {
   return (
-    <div className="footer bg-neutral-50 text-default-600 p-10">
+    <div className="footer text-sm bg-neutral-50 text-default-600 p-10">
       <p>
         Built by{" "}
         <Link
           href="https://www.priscillalynn.me/"
           color="foreground"
           isExternal
+          underline="hover"
+          className="text-sm"
         >
           Priscilla Colon.
         </Link>
