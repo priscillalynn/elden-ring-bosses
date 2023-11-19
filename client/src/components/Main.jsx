af6d887669d7d062e24264998a86549fe04c0dbd
priscillalynn <plynn9036@gmail.com>
2023-11-19 12:26:26 -0500

    added boss grid to main section

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,7 +1,9 @@
+import BossGrid from "./BossGrid";
+
 const Main = () => {
   return (
-    <div className="p-28">
-      <h2>Boss cards will render here</h2>
+    <div className="p-24">
+      <BossGrid />
     </div>
   );
 };
