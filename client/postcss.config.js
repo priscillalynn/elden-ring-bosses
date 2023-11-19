180d4d5845596e649cc6b027cafa0197ac1703dc
priscillalynn <plynn9036@gmail.com>
2023-11-18 21:45:46 -0500

    tailwind configuration

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,4 +1,6 @@
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
-
-export const plugins = [tailwindcss, autoprefixer];
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,12 @@
+const { nextui } = require("@nextui-org/react");
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     container: {
       center: true,
@@ -8,5 +14,9 @@ export default {
     extend: {},
   },
   darkMode: "class",
-  plugins: [],
+  plugins: [
+    nextui(),
+    require("tailwind-container-break-out"),
+    require("@tailwindcss/typography"),
+  ],
 };
