00adcec2b2fd3db5339ce2cf501452e71091b142
priscillalynn <plynn9036@gmail.com>
2023-11-19 20:02:14 -0500

    added react query

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,50 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import './styles/index.css';
+import "./styles/index.css";
+import {
+  MutationCache,
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "react-query";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// react-query
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+  queryCache: new QueryCache({
+    // global error handling for useQuery
+    onError: (error) => {
+      const responseMessage = error?.response?.data?.message;
+
+      toast.error(
+        `Error: ${error.message} \n\nMessage: ${responseMessage || "None"}`,
+      );
+    },
+  }),
+  mutationCache: new MutationCache({
+    // global error handling for useMutation
+    onError: (error) => {
+      const responseMessage = error?.response?.data?.message;
+
+      toast.error(
+        `Error: ${error.message} \n\nMessage: ${responseMessage || "None"}`,
+      );
+    },
+  }),
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
