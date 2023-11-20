05d8009b93b151d23158b9517ee28b3cc44efa1f
priscillalynn <plynn9036@gmail.com>
2023-11-20 10:05:13 -0500

    set up 404

--- client/src/pages/PageNotFound.jsx
+++ client/src/pages/PageNotFound.jsx
@@ -1,10 +1,14 @@
-/* eslint-disable no-unused-vars */
-import React from 'react'
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const PageNotFound = () => {
   return (
-    <div>404 Page Not Found</div>
-  )
-}
+    <>
+      <Header />
+      <div className="p-48 font-semibold text-3xl">Oops! 404 Page Not Found</div>
+      <Footer />
+    </>
+  );
+};
 
-export default PageNotFound
+export default PageNotFound;
