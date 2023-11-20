0e9420ee8349e3c629b3098b3692d4bd08c573aa
priscillalynn <plynn9036@gmail.com>
2023-11-19 20:01:43 -0500

    added filter section

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,10 +1,34 @@
 import BossGrid from "./BossGrid";
+import { Input, Select, SelectItem } from "@nextui-org/react";
+import { RouteOff, Search } from "lucide-react";
+import { useQuery, useQueryClient } from "react-query";
 
 const Main = () => {
+  const bossTypes = ["All", "Major", "Minor"];
+
   return (
-    <div className="p-24">
-      <BossGrid />
-    </div>
+    <>
+      <div className="flex gap-3 mx-20 sm:w-1/2 p-4 mt-8 rounded-xl">
+        <Select label="Boss Type" className="max-w-xs" size="sm">
+          {bossTypes.map((boss) => (
+            <SelectItem>{boss}</SelectItem>
+          ))}
+        </Select>
+        <Input
+          isClearable
+          placeholder="Search for a boss..."
+          variant="filled"
+          size="sm"
+          type="text"
+          className="rounded-xl"
+          startContent={<Search size={20} />}
+        />
+        <RouteOff size={44} cursor={"pointer"} />
+      </div>
+      <div className="mb-10 px-24 pt-8">
+        <BossGrid />
+      </div>
+    </>
   );
 };
 
