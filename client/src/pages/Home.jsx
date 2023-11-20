c17e170f5a1cf034b712dbd4c304447d258ae9ea
priscillalynn <plynn9036@gmail.com>
2023-11-20 10:05:05 -0500

    set up home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,15 @@
+import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Main from "../components/Main";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+};
 
-export default Home
+export default Home;
