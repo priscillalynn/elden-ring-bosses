09d9c5a44fc3bb64f108565d80220ada548b7b4f
priscillalynn <plynn9036@gmail.com>
2023-11-19 20:02:00 -0500

    changed margin

--- client/src/components/BossCard.jsx
+++ client/src/components/BossCard.jsx
@@ -21,7 +21,7 @@ const BossCard = ({ boss }) => {
         src={boss.image}
       />
       <CardFooter className="justify-end text-white before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)]">
-        <p className="z-10 mr-10 text-center font-bold">{boss.name}</p>
+        <p className="z-10 mr-5 text-center font-bold">{boss.name}</p>
         <Button
           className="text-tiny text-white bg-black/30 z-10"
           variant="flat"
