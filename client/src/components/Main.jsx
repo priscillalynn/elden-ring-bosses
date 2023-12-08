fe085201339e51771dde29c37d6a296cd88a11bf
priscillalynn <plynn9036@gmail.com>
2023-12-07 22:38:37 -0500

    added modal

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,12 +1,24 @@
 import BossGrid from "./BossGrid";
-import { Button, Input, Select, SelectItem } from "@nextui-org/react";
+import {
+  Button,
+  Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  Select,
+  SelectItem,
+  useDisclosure,
+} from "@nextui-org/react";
 import { PlusCircle, RouteOff, Search } from "lucide-react";
-import { useQuery, useQueryClient } from "react-query";
-
 
 /* FILTERS */
 const Main = () => {
   const bossTypes = ["All", "Required", "Optional"];
+
+  /* ADD NEW BOSS MODAL */
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   return (
     <>
@@ -28,9 +40,50 @@ const Main = () => {
         <RouteOff size={44} cursor={"pointer"} />
 
         {/* ADD BOSS */}
-        <Button color="default" endContent={<PlusCircle />}>
+        <Button color="default" endContent={<PlusCircle />} onPress={onOpen}>
           ADD
-        </Button>{" "}
+        </Button>
+
+        {/* ADD BOSS MODAL */}
+        <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+          <ModalContent>
+            {(onClose) => (
+              <>
+                <ModalHeader className="flex flex-col gap-1">
+                  Modal Title
+                </ModalHeader>
+                <ModalBody>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nullam pulvinar risus non risus hendrerit venenatis.
+                    Pellentesque sit amet hendrerit risus, sed porttitor quam.
+                  </p>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nullam pulvinar risus non risus hendrerit venenatis.
+                    Pellentesque sit amet hendrerit risus, sed porttitor quam.
+                  </p>
+                  <p>
+                    Magna exercitation reprehenderit magna aute tempor cupidatat
+                    consequat elit dolor adipisicing. Mollit dolor eiusmod sunt
+                    ex incididunt cillum quis. Velit duis sit officia eiusmod
+                    Lorem aliqua enim laboris do dolor eiusmod. Et mollit
+                    incididunt nisi consectetur esse laborum eiusmod pariatur
+                    proident Lorem eiusmod et. Culpa deserunt nostrud ad veniam.
+                  </p>
+                </ModalBody>
+                <ModalFooter>
+                  <Button color="danger" variant="light" onPress={onClose}>
+                    Close
+                  </Button>
+                  <Button color="primary" onPress={onClose}>
+                    Action
+                  </Button>
+                </ModalFooter>
+              </>
+            )}
+          </ModalContent>
+        </Modal>
       </div>
 
       {/* BOSS GRID DISPLAY */}
